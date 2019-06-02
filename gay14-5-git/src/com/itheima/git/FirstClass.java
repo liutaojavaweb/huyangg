bded5f6d400752f54c2827cebaa9891c78a21c61
胡杨 <huyang@itcast.cn>
2019-06-02 22:21:12 +0800

    git

---
+++ gay14-5-git/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,28 @@
+package com.itheima.git;
+
+/**
+ * FirstClass class
+ * git
+ *
+ * @Author: 刘韬
+ * @Date: 2019/6/2
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello Git");
+            System.out.println("Hello Git");
+            System.out.println("Hello Git");
+
+        }
+
+
+    }
+
+    public static void a() {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+
+}
---
+++ gay14-5-git/src/com/itheima/git/a.java
@@ -0,0 +1,22 @@
+package com.itheima.git;
+
+/**
+ * class
+ *
+ * @Author: 刘韬
+ * @Date: 2019/6/2
+ */
+public class a {
+
+    public static void main(String[] args) {
+        System.out.println("sdfdsf");
+    }
+
+
+
+
+
+    public void s (){
+        System.out.println("sdfsgjhdklsgjogjoslg");
+    }
+}
